d4000e13ff30e34f715c8a278ba3a0240c2e623f
DATBOI <alexsimons9999@gmail.com>
2019-01-22 18:32:36 -0600

    I see your base component and raise you a way to get home

--- ui/src/components/BaseTopicView.js
+++ ui/src/components/BaseTopicView.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {withStyles} from '@material-ui/core/styles';
+import withRouter from "react-router/es/withRouter";
+import Link from "react-router-dom/es/Link";
 
 const styles = theme => ({
   listItem: {
@@ -11,11 +13,13 @@ const styles = theme => ({
 function BaseTopicView(props) {
   return (
     <div>
-      <div>Home</div>
+      <Link to={'/'} style={{textDecoration: 'none'}}>
+        Home
+      </Link>
       {props.children}
       <div>Feeter</div>
     </div>
   );
 }
 
-export default withStyles(styles)(BaseTopicView);
+export default withRouter(withStyles(styles)(BaseTopicView));
