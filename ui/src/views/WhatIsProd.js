c388afb44594b94efc49646eb9a4003287cf093e
DATBOI <alexsimons9999@gmail.com>
2019-01-23 18:35:25 -0600

    So this is how I can markdown ._.

--- ui/src/views/WhatIsProd.js
+++ ui/src/views/WhatIsProd.js
@@ -1,16 +1,30 @@
 import BaseTopicView from "../components/BaseTopicView";
 import React from "react";
 import withStyles from "@material-ui/core/es/styles/withStyles";
+import Markdown from "../components/Markdown";
+
 
 const styles = theme => ({
   thing: {
     color: 'red'
-  }
+  },
+  markdown: {
+    padding: `${theme.spacing.unit * 3}px 0`,
+    whiteSpace: 'preserve',
+  },
 });
+
+const markdown = `
+# yolo
+## reps 4 jesus
+`;
 
 const WhatIsProd = ({classes}) => (
   <BaseTopicView classes={classes}>
     <div className={classes.thing}>Ello govna</div>
+    <Markdown className={classes.markdown}>
+      {markdown}
+    </Markdown>
   </BaseTopicView>
 );
 
