dbb023851309ec59b31b06ed017853d5bbc327f6
DATBOI <alexsimons9999@gmail.com>
2019-01-24 18:44:08 -0600

    I̛͕̞̝͚̹ ͉̝̹͇̦ͅa̱̙̱̙m͉̙̣̹͘ ͇̫̭̟̻̤b̩͇̲͚̘̭͢e̶͎͇̦̖ͅc̦̯͍oḿe̺͔͈̬̪̹ ͖̜̫͖̳̣d̡̘e̯ạ͙̬̫̤͈̩t̳̥̞̪̬h̯͞.̶̰̟̫͉͕̼

--- ui/src/views/DevelopLocalBody.js
+++ ui/src/views/DevelopLocalBody.js
@@ -27,7 +27,20 @@ The use of Host-Headers will allow NGINX to proxy requests based off of the doma
 Allowing for a single server to handle requests for many domains.
 
 The server will also handle all graceful failure pages that the user may see while interacting with your domain.
-                
+
+#### The React Application
+Will be accessible from https://static-site.localhost
+The static content will be served by the NGINX server
+
+#### The Spring Application
+Will be accessible from https://compute-app-one.localhost
+The static content will be served by the Spring Boot app via the NGINX proxy server.
+The NGINX server will access the spring boot server via the Docker supplied host name: **compute-app-one**
+
+#### The Express Application
+Will be accessible from https://compute-app-two.localhost
+The static content will be served by the Express app via the NGINX proxy server.
+The NGINX server will access the spring boot server via the Docker supplied host name: **compute-app-two**
 
 
 ![Local Architecture](https://raw.githubusercontent.com/cyclic-reference/frugal-aws-ui/master/presentation/images/LOCAL_ARCH.png)
