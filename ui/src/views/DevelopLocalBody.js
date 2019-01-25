f2fa58b9545a26c67b74cc5cb0a458e7a8e87f21
DATBOI <alexsimons9999@gmail.com>
2019-01-24 18:38:07 -0600

    Cool story bro

--- ui/src/views/DevelopLocalBody.js
+++ ui/src/views/DevelopLocalBody.js
@@ -1,8 +1,19 @@
 const markdown = `
 ## Replicate Production on Local Machine
 
-- Organize all of your projects.
+### Example Project Setup
+There will be a total of 3 websites:
 
+- A React Web Application
+- A Spring Boot Application
+- A Express Application
+
+### Entry Point
+The entry point to all of our application will be the NGINX server.
+All Client-Side communications will be in HTTPS.
+
+
+![Local Architecture](https://raw.githubusercontent.com/cyclic-reference/frugal-aws-ui/master/presentation/images/LOCAL_ARCH.png)
 
 `;
 
