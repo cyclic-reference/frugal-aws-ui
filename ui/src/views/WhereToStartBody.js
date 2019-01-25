1eae89b22679f44a3e5daee6ca3a4aa9558de9ca
DATBOI <alexsimons9999@gmail.com>
2019-01-24 18:19:08 -0600

    Just some static content and webservice stuff

--- ui/src/views/WhereToStartBody.js
+++ ui/src/views/WhereToStartBody.js
@@ -15,6 +15,15 @@ Gather all of your _web-based_ projects and put them into one of two categories:
 - Static Content
 - Web Services
 
+#### Static Content
+Just a simple html based application that may or may not have a corresponding backend/REST API.
+
+#### Web Services
+Projects that run on/are servers and normally are accessed from \`localhost:8080\` or something like that.
+These may or may not serve static content as well
+
+**The distinction between these two categories is the amount of $$ you will spend*
+
 ### Install Docker on your local machine
 ### Acquire AWS Account
 ### Combine the above
