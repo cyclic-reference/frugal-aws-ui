7313fd42e58ff6b01eb2ac3aa2b6805f69fcf684
DATBOI <alexsimons9999@gmail.com>
2019-01-27 09:56:44 -0600

    Updated the verbage on the local development and supplied a link to the exapmle project that implements what this article suggests

--- ui/src/views/DevelopLocalBody.js
+++ ui/src/views/DevelopLocalBody.js
@@ -2,7 +2,7 @@ const markdown = `
 ## Replicate Production on Local Machine
 
 ### Example Project Setup
-There will be a total of 3 websites:
+There will be a total of 3 websites in this demonstration project:
 
 - A React Web Application
 - A Spring Boot Application
@@ -10,23 +10,21 @@ There will be a total of 3 websites:
 
 ### Entry Point
 The entry point to all of our application will be the NGINX server.
-All Client-Side communications will be in HTTPS.
+The server will be configured such that all Client-Side communications will be in HTTPS.
 
 #### What is NGINX?
 NGINX is open source software for serving static content, proxying requests, and more things.
-Static content and proxying are just the ones focused on right now.
+Static content and proxying are just the features that we will be focused on right now.
 
 ###### NGINX Configuration
 
+Self-Signed certificates can be used for initially setting up secured communications.
+Which will later be replaced by free, trusted [Let's Encrypt](https://letsencrypt.org/) certificates.
 
-Self-Signed certificates can be used for the initial HTTPS set up.
-Which will later be replaced by free, trusted [Let's Encrypt](https://github.com/certbot/certbot/tree/master/certbot-dns-route53) certificates.
+In order have a single server handle the requests across many of your domains, NGINX will have to be configured to take advantage of [Host-Headers](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Host).
+Allowing for a single server to handle requests for many domains, which means that you have to pay less.
 
-
-The use of Host-Headers will allow NGINX to proxy requests based off of the domain supplied.
-Allowing for a single server to handle requests for many domains.
-
-The server will also handle all graceful failure pages that the user may see while interacting with your domain.
+This server will also handle all graceful failure pages that the user may see while interacting with your domain.
 
 #### The React Application
 Will be accessible from https://static-site.localhost
@@ -44,6 +42,11 @@ The NGINX server will access the spring boot server via the Docker supplied host
 
 
 ![Local Architecture](https://raw.githubusercontent.com/cyclic-reference/frugal-aws-ui/master/presentation/images/LOCAL_ARCH.png)
+
+
+An example implementation of the schema can be found here:
+
+## [https://github.com/cyclic-reference/frugal-aws](https://github.com/cyclic-reference/frugal-aws) 
 
 `;
 
