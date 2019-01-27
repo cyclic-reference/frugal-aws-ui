be7276b806913c7c1405bbc27fc009dec2f78e52
DATBOI <alexsimons9999@gmail.com>
2019-01-27 11:01:09 -0600

    Moar orange.

--- ui/src/components/Footer.js
+++ ui/src/components/Footer.js
@@ -1,5 +1,5 @@
 import Typography from "@material-ui/core/es/Typography/Typography";
-import Reach from "../images/reach_yellow.svg";
+import Reach from "../images/reach_orange.svg";
 import React from "react";
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
